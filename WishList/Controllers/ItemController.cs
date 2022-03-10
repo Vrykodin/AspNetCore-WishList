ad0b2127a64653cf169571be24951470577bbaf9
Chris Alberty <17458734+Vrykodin@users.noreply.github.com>
2022-03-09 19:27:44 -0600

    Adding ItemController

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("/Item/Index", _context.Items);
+        }
+    }
+}
